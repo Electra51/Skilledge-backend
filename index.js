bba223da7828c0af142b008e885e4fbec7c6432a
safayet nur <safayetnurelectra@gmail.com>
2025-12-09 14:47:38 +0600

    update: update index and other folder

--- index.js
+++ index.js
@@ -1,61 +1,67 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5001;
 
-app.use(cors())
+app.use(cors());
 
-const categories = require('./Data/categories.json');
-const news = require('./Data/news.json');
+const categories = require("./Data/categories.json");
+const news = require("./Data/news.json");
 
-
-app.get('/', (req, res) => {
-    res.send('category running')
+app.get("/", (req, res) => {
+  res.send("category running");
 });
 
-app.get('/categories', (req, res) => {
-    res.send(categories)
-})
-app.get('/news', (req, res) =>{
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/category/:id/checkout", (req, res) => {
+  const id = req.params.id;
+
+  const category_news = news.filter((n) => n.category_id === id);
+  res.send(category_news);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const course = news.find((n) => n._id === id);
+
+  if (course) {
+    res.send(course);
+  } else {
+    res.status(404).send({ message: "Course not found" });
+  }
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "06") {
     res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+app.get("/checkout/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "06") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
 });
 
-
-app.get('/category/:id/checkout', (req, res) => {
-    const id = req.params.id;
-    
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    
-})
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '06') {
-        res.send(news);
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    }
-})
-app.get('/checkout/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '06') {
-        res.send(news);
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    }
-})
-
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
-    
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
-    console.log('server running on port',port)
-})
+  console.log("server running on port", port);
+});
